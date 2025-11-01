bd97b16cd85550a0248762c8bbbb43677e166a2e
Humberto Ribeiro <betofoxmal@gmail.com>
2025-10-31 21:08:52 -0300

    feat: update list users and admins button active deleted

--- src/components/breadcrumbs/update-user-breadcrumb.tsx
+++ src/components/breadcrumbs/update-user-breadcrumb.tsx
@@ -4,25 +4,30 @@ import { useEffect } from 'react';
 import { useBreadcrumbs } from '@/context/breadcrumb-context';
 import { type User } from '@/types';
 import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/navigation';
 
 export default function UpdateUserBreadcrumb({ user }: { user: User }) {
+    const router = useRouter();
     const { setBreadcrumbs } = useBreadcrumbs();
     const tb = useTranslations('Breadcrumb');
     useEffect(() => {
-        const base = [
-            { title: tb('Dashboard'), href: '/dashboard' },
-            { title: tb('Administrators'), href: '/dashboard/admins' },
-        ];
+        if (user) {
+            const base = [
+                { title: tb('Dashboard'), href: '/dashboard' },
+                { title: tb('Administrators'), href: '/dashboard/admins' },
+            ];
 
-        const nameOrDefault = user?.name ? `${tb('Update')} ${user.name}` : tb('UpdateUser');
-        const updateCrumb = { title: nameOrDefault, href: `/dashboard/admins/update/${user.id}` };
+            const nameOrDefault = user?.name ? `${tb('Update')} ${user.name}` : tb('UpdateUser');
+            const updateCrumb = { title: nameOrDefault, href: `/dashboard/admins/update/${user.id}` };
 
-        const crumbs =
-            user.role === 'USER'
-                ? [...base, { title: tb('Users'), href: '/dashboard/admins/users' }, updateCrumb]
-                : [...base, updateCrumb];
+            const crumbs =
+                user.role === 'USER'
+                    ? [...base, { title: tb('Users'), href: '/dashboard/admins/users' }, updateCrumb]
+                    : [...base, updateCrumb];
 
-        setBreadcrumbs(crumbs);
+            setBreadcrumbs(crumbs);
+        }
+        router.back();
     }, [setBreadcrumbs, user, tb]);
 
     return null;
