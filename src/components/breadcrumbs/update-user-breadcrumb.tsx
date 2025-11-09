b0eaba68284a72774fe677f3fb62653fed78484d
Humberto Ribeiro <betofoxmal@gmail.com>
2025-11-09 04:10:45 -0300

    fix: redirect update user

--- src/components/breadcrumbs/update-user-breadcrumb.tsx
+++ src/components/breadcrumbs/update-user-breadcrumb.tsx
@@ -26,8 +26,9 @@ export default function UpdateUserBreadcrumb({ user }: { user: User }) {
                     : [...base, updateCrumb];
 
             setBreadcrumbs(crumbs);
-        }
-        router.back();
+        } else {
+            router.back();
+        };
     }, [setBreadcrumbs, user, tb]);
 
     return null;
